8600ac20d614a97f61c5dde7cded1a6f818d37e8
Niranjan Chhawdi <chhawdiniranjan@gmail.com>
2025-03-05 14:33:42 -0800

    Update damage.cs

--- Game290 Team2/Assets/damage.cs
+++ Game290 Team2/Assets/damage.cs
@@ -7,18 +7,6 @@ public class Damage : MonoBehaviour
 
     public PlayerHealth phealth;
     public float damage;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D other)
 
     {
