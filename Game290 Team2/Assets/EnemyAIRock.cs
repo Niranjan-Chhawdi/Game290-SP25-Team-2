ef11372945e2c3e5b253309b360e5148f9e2c59d
Jacob <jacobbdms12@gmail.com>
2025-03-19 06:15:23 -0700

    Abilities
    All abilities, stun needs work

--- Game290 Team2/Assets/EnemyAIRock.cs
+++ Game290 Team2/Assets/EnemyAIRock.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class EnemyStun : MonoBehaviour
 {
     private bool isStunned = false;
-    private float stunDuration = 2f;  // How long the enemy stays stunned
+    private float stunDuration = 2f;  
 
-    // Call this method to stun the enemy
+ 
     public void StunEnemy()
     {
         if (!isStunned)
@@ -15,10 +15,7 @@ public class EnemyStun : MonoBehaviour
             isStunned = true;
             Debug.Log("Enemy Stunned!");
 
-            // You can add more logic here, like stopping the enemy's movement
-            // For example, if you have a movement script for the enemy, you could disable it temporarily
-
-            // Call UnstunEnemy after the stun duration
+            
             Invoke(nameof(UnstunEnemy), stunDuration);
         }
     }
@@ -28,10 +25,10 @@ public class EnemyStun : MonoBehaviour
         isStunned = false;
         Debug.Log("Enemy Unstunned!");
 
-        // Re-enable movement or any other behavior when the enemy is unstunned
+        
     }
 
-    // Optionally, you can use this method to check if the enemy is stunned
+    
     public bool IsStunned()
     {
         return isStunned;
--- Game290 Team2/Assets/Scripts/Player/Player Controller.cs
+++ Game290 Team2/Assets/Scripts/Player/Player Controller.cs
@@ -5,11 +5,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 1f;
-
+    private float moveSpeed = 1f;
+    public AbilityManager abilityManager;
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        abilityManager = FindObjectOfType<AbilityManager>();
+
+        if (abilityManager != null)
+        {
+            Debug.Log("AbilityManager found!");
+        }
+        else
+        {
+            Debug.LogError("AbilityManager not found in the scene.");
+        }
+    }
 
     private void Awake()
     {
@@ -24,7 +39,26 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        PlayerInput();
+        // Check for input to use abilities
+        if (Input.GetKeyDown(KeyCode.Alpha1))  // Press '1' to use Stun
+        {
+            abilityManager.UseAbility(0, gameObject);  // 0 is for StunAbility
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))  // Press '4' to use Teleport
+        {
+            abilityManager.UseAbility(1, gameObject);  // 1 is for TeleportAbility
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))  // Press '3' to use Remove Enemy
+        {
+            abilityManager.UseAbility(2, gameObject);  // 2 is for RemoveEnemyAbility
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))  // Press '2' to use Invisibility
+        {
+            abilityManager.UseAbility(3, gameObject);  // 3 is for InvisibilityAbility
+        }
     }
 
     private void FixedUpdate()
@@ -34,14 +68,16 @@ public class PlayerController : MonoBehaviour
 
     private void PlayerInput()
     {
+        // Capture movement input
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
     }
 
     private void Move()
     {
-    rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        // Apply movement to the rigidbody
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
 
-    // Rotate player based on movement direction
+        // Rotate player based on movement direction
         if (movement != Vector2.zero)
         {
             float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
