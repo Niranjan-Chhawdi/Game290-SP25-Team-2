4dc995de97cd6e8cf707d2237034f83f5fb374d5
Jacob <jacobbdms12@gmail.com>
2025-04-02 04:40:57 -0700

    More changes

--- Game290 Team2/Assets/Scripts/Abilities/Dash.cs
+++ Game290 Team2/Assets/Scripts/Abilities/Dash.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//reference =https://www.youtube.com/watch?v=ry4I6QyPw4E&t=12s
+
 [CreateAssetMenu]
 public class Dash : Ability
 {
